0a018c7dfac2abb943b12def8551ac0a0daa8d97
Brian Hyams <bghyams@gmail.com>
2020-04-16 12:12:16 -0500

    Override version to be 0.0.1
    Nuget publish succeeded, but I don't see it in nuget.  The log output makes me wonder if the build # is problematic:
    > 12:01:45 Build successfully deployed. Browse it in Artifactory under https://csdisco.jfrog.io/csdisco/webapp/builds/csdisco-ical-net%20::%20PR-1/3

--- net-core/build.ps1
+++ net-core/build.ps1
@@ -23,7 +23,7 @@ function build() {
 }
 
 function package() {
-    dotnet pack Ical.Net/Ical.Net.csproj -p:NuspecFile=../Ical.Net.nuspec -p:NuspecBasePath=../ -p:NuspecProperties=\"version=0.0.${env:BUILD_NUMBER}\"
+    dotnet pack Ical.Net/Ical.Net.csproj -p:NuspecFile=../Ical.Net.nuspec -p:NuspecBasePath=../ -p:NuspecProperties=\"version=0.0.1\"
     if ($LASTEXITCODE) {
         throw "Error package"
     }
